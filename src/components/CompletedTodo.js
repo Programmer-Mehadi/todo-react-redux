3e04e71b28b486312d2c03b55aaf652864165e48
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-03-28 14:12:16 +0600

    make table responsive.

--- src/components/CompletedTodo.js
+++ src/components/CompletedTodo.js
@@ -9,49 +9,51 @@ const CompletedTodo = () => {
       <h2 className="font-bold text-xl bg-green-800 rounded-t-md p-5 text-white">
         Completed Todo
       </h2>
-      <table className="table w-[98%] mx-auto my-5">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Status</th>
-            <th>Todo</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.length > 0 ? (
-            data?.map((todo, index) => {
-              const { todoText, todoDate } = todo;
-              return (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>
-                    <input
-                      type="checkbox"
-                      className="w-[30px]"
-                      checked={true}
-                      onChange={() => {
-                        dispatch({
-                          type: "MAKE_INCOMPLETE_TODO",
-                          payload: { id: todo.id },
-                        });
-                      }}
-                    />
-                  </td>
-                  <td className="text-lg">{todoText}</td>
-                  <td className="text-lg">{todoDate}</td>
-                </tr>
-              );
-            })
-          ) : (
+      <div className="overflow-auto">
+        <table className="table w-[98%] mx-auto my-5">
+          <thead>
             <tr>
-              <td className="text-lg text-slate-500 text-center" colspan="4">
-                No Completed Todo
-              </td>
+              <th>No</th>
+              <th>Status</th>
+              <th>Todo</th>
+              <th>Date</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data?.length > 0 ? (
+              data?.map((todo, index) => {
+                const { todoText, todoDate } = todo;
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        className="w-[30px]"
+                        checked={true}
+                        onChange={() => {
+                          dispatch({
+                            type: "MAKE_INCOMPLETE_TODO",
+                            payload: { id: todo.id },
+                          });
+                        }}
+                      />
+                    </td>
+                    <td className="text-lg">{todoText}</td>
+                    <td className="text-lg">{todoDate}</td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td className="text-lg text-slate-500 text-center" colspan="4">
+                  No Completed Todo
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
--- src/components/IncompleteTodo.js
+++ src/components/IncompleteTodo.js
@@ -10,48 +10,50 @@ const IncompleteTodo = () => {
       <h2 className="font-bold text-xl bg-rose-800 rounded-t-md p-5 text-white">
         Incomplete Todo
       </h2>
-      <table className="table w-[98%] mx-auto my-5">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Status</th>
-            <th>Todo</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.length > 0 ? (
-            data?.map((todo, index) => {
-              const { todoText, todoDate } = todo;
-              return (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>
-                    <input
-                      type="checkbox"
-                      className="w-[30px]"
-                      onChange={() => {
-                        dispatch({
-                          type: "MAKE_COMPLETED_TODO",
-                          payload: { id: todo.id },
-                        });
-                      }}
-                    />
-                  </td>
-                  <td className="text-lg">{todoText}</td>
-                  <td className="text-lg">{todoDate}</td>
-                </tr>
-              );
-            })
-          ) : (
+      <div className="overflow-auto">
+        <table className="table w-[98%] mx-auto my-5">
+          <thead>
             <tr>
-              <td className="text-lg text-slate-500 text-center" colspan="4">
-                No Incompleted Todo
-              </td>
+              <th>No</th>
+              <th>Status</th>
+              <th>Todo</th>
+              <th>Date</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data?.length > 0 ? (
+              data?.map((todo, index) => {
+                const { todoText, todoDate } = todo;
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        className="w-[30px]"
+                        onChange={() => {
+                          dispatch({
+                            type: "MAKE_COMPLETED_TODO",
+                            payload: { id: todo.id },
+                          });
+                        }}
+                      />
+                    </td>
+                    <td className="text-lg">{todoText}</td>
+                    <td className="text-lg">{todoDate}</td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td className="text-lg text-slate-500 text-center" colspan="4">
+                  No Incompleted Todo
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
